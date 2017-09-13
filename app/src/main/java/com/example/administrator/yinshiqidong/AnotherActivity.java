b6f5f7d03bf6ead370583e0a7d52577b90c41765
lylznb <283186987@qq.com>
2017-09-13 19:02:03 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/yinshiqidong/AnotherActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.yinshiqidong;
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+
+public class AnotherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+
+}
